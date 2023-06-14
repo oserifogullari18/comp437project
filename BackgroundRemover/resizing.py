32338b3eb512cdf32e395da22e1a13a2fb8076e7
oserifogullari18 <oserifogullari18@ku.edu.tr>
2023-06-14 20:59:11 +0300

    Background and pose esmation python scripts & workoutApp

---
+++ BackgroundRemover/resizing.py
@@ -0,0 +1,11 @@
+import cv2
+import os
+
+for root, subdirs, files in os.walk('/Users/ozlemserifogullari/Documents/comp437/project/BackgroundRemover/bgimages'):
+    for f in files:
+        if f.endswith('jpg'):
+            # print(f)
+            img = cv2.imread('/Users/ozlemserifogullari/Documents/comp437/project/BackgroundRemover/bgimages/' + f)
+            img = cv2.resize(img, (640, 480))
+            cv2.imwrite('/Users/ozlemserifogullari/Documents/comp437/BackgroundRemover/bgimages/'+f, img)
+            print(*["Image", f, "is resized to 640 X 480"])
