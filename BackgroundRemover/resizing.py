511d3bd799bf5797be33baaa332c6e44e3af9b29
oserifogullari18 <oserifogullari18@ku.edu.tr>
2023-06-15 11:30:04 +0300

    fixed resizing and ipynb file

--- BackgroundRemover/resizing.py
+++ BackgroundRemover/resizing.py
@@ -7,5 +7,5 @@ for root, subdirs, files in os.walk('/Users/ozlemserifogullari/Documents/comp437
             # print(f)
             img = cv2.imread('/Users/ozlemserifogullari/Documents/comp437/project/BackgroundRemover/bgimages/' + f)
             img = cv2.resize(img, (640, 480))
-            cv2.imwrite('/Users/ozlemserifogullari/Documents/comp437/BackgroundRemover/bgimages/'+f, img)
+            cv2.imwrite('/Users/ozlemserifogullari/Documents/comp437/project/BackgroundRemover/bgimages/'+f, img)
             print(*["Image", f, "is resized to 640 X 480"])
